fa7fc331bde5676c4644d1cf655eab326a3a2234
sean <seanyi433@gmail.com>
2023-03-23 21:15:28 -0400

    lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+function findMatching(driver,string) {
+  let car = driver.filter(drivers=> drivers.toUpperCase()===string.toUpperCase())
+return car
+}
+
+function fuzzyMatch(driver,string){
+  let car = driver.filter(drivers=>drivers.indexOf(string) ==0)
+  return car
+} 
+
+function matchName(driver,string){
+  let car = driver.filter(drivers=> drivers.name === string)
+  return car
+}
